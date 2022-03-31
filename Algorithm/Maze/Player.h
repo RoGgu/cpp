7c7140b17c976d0ad15a84315eff5a8b1087f12f
AnSungHun <cjswo4577@gmail.com>
2022-03-31 18:48:14 +0900

    오른손 법칙
    - Player 이동 로직 작업
    - 오른손 법칙적용해서 이동하도록 설정
    - Update는 설정한 Tick 시간이 될 때마다 이동하도록 설정

--- Algorithm/Maze/Player.h
+++ Algorithm/Maze/Player.h
@@ -4,6 +4,11 @@ class Board;
 
 class Player
 {
+    enum
+    {
+        MOVE_TICK = 100 // 몇밀리세컨드마다 이동하게 할지
+    };
+
 public:
     void Init(Board* board);
     void Update(uint64 deltaTick);
@@ -11,11 +16,16 @@ public:
     void SetPos(Pos pos) { _pos = pos; }
     Pos GetPos() { return _pos; }
 
+    bool CanGo(Pos pos);
+
 private:
     Pos _pos = {};
     int32 _dir = DIR_UP;
     Board* _board = nullptr;
 
+    vector<Pos> _path;
+    uint32   _pathIndex = 0;
+    uint64 _sumTick = 0; // tick연산 
 
 };
 
