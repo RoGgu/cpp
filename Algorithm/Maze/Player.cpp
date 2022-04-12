892148d2aebfa61f3058a1a3c73507a2ebe7beb1
AnSungHun <cjswo4577@gmail.com>
2022-04-12 11:51:15 +0900

    BFS를 이용한 길찾기 구현
    - 기존 오른손 법칙을 통한 길 찾기 방식을 BFS를 통해서 찾도록 변경

--- Algorithm/Maze/Player.cpp
+++ Algorithm/Maze/Player.cpp
@@ -8,14 +8,41 @@ void Player::Init(Board* board)
     _pos = board->GetEnterPos();
     _board = board;
 
+    //RightHand();
+    Bfs();
+}
+
+void Player::Update(uint64 deltaTick)
+{
+    if (_pathIndex >= _path.size())
+        return;
+
+    _sumTick += deltaTick;
+    if (_sumTick >= MOVE_TICK)
+    {
+        _sumTick = 0;
+
+        _pos = _path[_pathIndex];
+        _pathIndex++;
+    }
+}
+
+bool Player::CanGo(Pos pos)
+{
+    TileType tileType = _board->GetTileType(pos);
+    return tileType == TileType::EMPTY;
+}
+
+void Player::RightHand()
+{
     Pos pos = _pos; // 이동 시뮬레이션 좌표
 
     _path.clear();
     _path.push_back(pos);
 
     // 목적지 도착하기 전에는 계속 실행
-    Pos dest = board->GetExitPos();
-    
+    Pos dest = _board->GetExitPos();
+
     Pos front[4] =
     {
         Pos {-1, 0},    // UP
@@ -52,7 +79,7 @@ void Player::Init(Board* board)
             _dir = (_dir + 1) % DIR_COUNT;
         }
     }
-    
+
     // 막다른길 가서 다시 되돌아오는 경로는 삭제한다
     stack<Pos> s;
     // 마지막 도착지 뺴고 다 검사
@@ -81,25 +108,78 @@ void Player::Init(Board* board)
     _path = path;
 }
 
-void Player::Update(uint64 deltaTick)
+void Player::Bfs()
 {
-    if (_pathIndex >= _path.size())
-        return;
+    Pos pos = _pos; // 이동 시뮬레이션 좌표
 
-    _sumTick += deltaTick;
-    if (_sumTick >= MOVE_TICK)
+    // 목적지 도착하기 전에는 계속 실행
+    Pos dest = _board->GetExitPos();
+
+    Pos front[4] =
     {
-        _sumTick = 0;
+        Pos {-1, 0},    // UP
+        Pos {0, -1},    // LEFT
+        Pos {1, 0},     // DOWN
+        Pos {0, 1},     // RIGHT
+    };
 
-        _pos = _path[_pathIndex];
-        _pathIndex++;
+    const int32 size = _board->GetSize();
+    vector<vector<bool>> discovered(size, vector<bool>(size, false));
+
+    //vector<vector<Pos>> parent;
+    // parent[A] = B; -> A는 B로 인해 발견함
+    map<Pos, Pos> parent;
+
+    queue<Pos> q;
+    q.push(pos);
+    discovered[pos.y][pos.x] = true;
+    parent[pos] = pos;
+
+    while (q.empty() == false)
+    {
+        pos = q.front();
+        q.pop();
+
+        // 방문!
+        if (pos == dest) // 목적지 도달하면 그만함
+            break;
+
+
+        
+        for (int32 dir = 0; dir < DIR_COUNT; dir++)
+        {
+            Pos nextPos = pos + front[dir]; // 다음 좌표
+            // 갈 수 있는 지역은 맞는지 확인
+            if (CanGo(nextPos) == false)
+                continue;
+
+            // 이미 발견한 지역인지 확인.
+            if (discovered[nextPos.y][nextPos.x])
+                continue;
+
+            q.push(nextPos);
+            discovered[nextPos.y][nextPos.x] = true;
+            parent[nextPos] = pos;
+        }
     }
+
+    _path.clear();
+
+    // 거꾸로 거슬러 올라간다.
+    pos = dest;
+
+    while (true)
+    {
+        _path.push_back(pos);
+
+        // 시작점은 자신이 곧 부모이다.
+        if (pos == parent[pos])
+            break;
+
+        pos = parent[pos];
+    }
+
+    // 부모로 꺼구로 들어가 있으니 반전시켜준다
+    std::reverse(_path.begin(), _path.end());
+    _path.push_back(pos);
 }
-
-bool Player::CanGo(Pos pos)
-{
-    TileType tileType = _board->GetTileType(pos);
-    return tileType == TileType::EMPTY;
-}
-
-
--- Algorithm/Maze/Player.h
+++ Algorithm/Maze/Player.h
@@ -17,6 +17,11 @@ public:
     Pos GetPos() { return _pos; }
 
     bool CanGo(Pos pos);
+    
+private:
+    void RightHand();
+    void Bfs();
+
 
 private:
     Pos _pos = {};
